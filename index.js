e852f414ba535bfbc08f41a833015db4d316df2c
delong <w.del@qq.com>
2023-04-07 17:48:28 +0800

    feat: add 2 more fonts, OPPO and Alibaba

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const Fontmin = require("fontmin");
 const text = require("./chars");
 
-const fontmin = new Fontmin()
+// 思源黑体
+new Fontmin()
   .src("./fonts/SourceHanSansCN-Medium.ttf")
   .use(Fontmin.glyph({ text }))
-  .dest("dest");
+  .dest("dist")
+  .run((err) => err && console.log(err));
 
-fontmin.run(function (err, files) {
-  if (err) console.log(err);
-});
+// 阿里巴巴普惠体
+new Fontmin()
+  .src("./fonts/Alibaba-PuHuiTi-Medium.ttf")
+  .use(Fontmin.glyph({ text }))
+  .dest("dist")
+  .run((err) => err && console.log(err));
+
+// OPPO 体
+new Fontmin()
+  .src("./fonts/OPPOSans-M.ttf")
+  .use(Fontmin.glyph({ text }))
+  .dest("dist")
+  .run((err) => err && console.log(err));
