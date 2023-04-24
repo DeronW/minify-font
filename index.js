d4efdd3b546942c95ee5a9e8a393e46bb669e944
delong <w.del@qq.com>
2023-04-24 15:09:23 +0800

    feat: 增加 空格 和 感叹号 两个字符
    1. 增加空格字符 https://github.com/DeronW/minify-font/issues/2
    2. 修复斜杠 "\" 字符被当做转义字符
    3. 增加感叹号 "!"
    4. demo.html 文件中增加到 3 种字体的对比
    5. 重新编译 3 种字体，更新字体内容

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const Fontmin = require("fontmin");
 const text = require("./chars");
 
-// 思源黑体
-new Fontmin()
-  .src("./fonts/SourceHanSansCN-Medium.ttf")
-  .use(Fontmin.glyph({ text }))
-  .dest("dist")
-  .run((err) => err && console.log(err));
+const fonts = [
+  // 思源黑体
+  "./fonts/SourceHanSansCN-Medium.ttf",
+  // 阿里巴巴普惠体
+  "./fonts/Alibaba-PuHuiTi-Medium.ttf",
+  // OPPO 体
+  "./fonts/OPPOSans-M.ttf",
+];
 
-// 阿里巴巴普惠体
-new Fontmin()
-  .src("./fonts/Alibaba-PuHuiTi-Medium.ttf")
-  .use(Fontmin.glyph({ text }))
-  .dest("dist")
-  .run((err) => err && console.log(err));
-
-// OPPO 体
-new Fontmin()
-  .src("./fonts/OPPOSans-M.ttf")
-  .use(Fontmin.glyph({ text }))
-  .dest("dist")
-  .run((err) => err && console.log(err));
+for (const font of fonts) {
+  new Fontmin()
+    .src(font)
+    .use(Fontmin.glyph({ text }))
+    .dest("dist")
+    .run((err) => err && console.log(err));
+}
